25582f04e6cdafd5a32d8ef353b49b1cf5a3cade
codeguru <codeguru@users.sourceforge.net>
2013-02-13 20:29:23 -0700

    Add ListView to ContentResolverActivity

--- ContentProviderExample/ContentResolverExample/src/example/contentresolver/ContentResolverActivity.java
+++ ContentProviderExample/ContentResolverExample/src/example/contentresolver/ContentResolverActivity.java
@@ -1,14 +1,15 @@
 package example.contentresolver;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class ContentResolverActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class ContentResolverActivity extends ListActivity {
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
