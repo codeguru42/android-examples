1e37dcb6b290ef16045e451e4d8f6cbf2169126d
codeguru <codeguru@users.sourceforge.net>
2013-02-17 23:05:54 -0700

    Add WTFPL license notices; add Activity class; fix test class

---
+++ LibraryTestExample/TestExample/src/codeguru/example/library/HelloLibraryActivityTest.java
@@ -0,0 +1,20 @@
+/*
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details.
+ */
+package codeguru.example.library;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ */
+public class HelloLibraryActivityTest extends ActivityInstrumentationTestCase2<HelloLibraryActivity> {
+
+    public HelloLibraryActivityTest() {
+        super(HelloLibraryActivity.class);
+    }
+
+}
