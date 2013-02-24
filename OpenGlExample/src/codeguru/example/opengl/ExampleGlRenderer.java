c2de522112995ef9e54f7df5e56fe164e0df80fb
codeguru <codeguru@users.sourceforge.net>
2013-02-23 18:09:33 -0700

    Implement methods in codeguru.example.opengl.ExampleGlRenderer

--- OpenGlExample/src/codeguru/example/opengl/ExampleGlRenderer.java
+++ OpenGlExample/src/codeguru/example/opengl/ExampleGlRenderer.java
@@ -7,6 +7,7 @@
  */
 package codeguru.example.opengl;
 
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -18,15 +19,14 @@ import javax.microedition.khronos.opengles.GL10;
 public class ExampleGlRenderer implements GLSurfaceView.Renderer {
 
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        GLES20.glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
     }
 
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        GLES20.glViewport(0, 0, width, height);
     }
 
     public void onDrawFrame(GL10 gl) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
     }
-
 }
