8fea934e602bb744293d7947c0840ab65c54b4a8
codeguru <codeguru@users.sourceforge.net>
2013-02-26 06:11:53 -0700

    create codeguru.example.testrunner.tests.TestRunnerActivityTest

---
+++ TestRunnerExample/src/codeguru/example/testrunner/tests/TestRunnerActivityTest.java
@@ -0,0 +1,37 @@
+/*
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details.
+ */
+package codeguru.example.testrunner.tests;
+
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+import codeguru.example.testrunner.TestRunnerActivity;
+import junit.framework.Assert;
+
+/**
+ *
+ * @author codeguru <codeguru@users.sourceforge.net>
+ */
+public class TestRunnerActivityTest extends ActivityInstrumentationTestCase2<TestRunnerActivity> {
+
+    public TestRunnerActivityTest() {
+        super(TestRunnerActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        this.activity = this.getActivity();
+    }
+
+    public void testPreconditions() {
+        Assert.assertNotNull(this.activity);
+    }
+
+    private Activity activity = null;
+}
