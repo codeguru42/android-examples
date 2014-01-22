ccf64b1ae04e0abd431c29581d370cd53d1315da
codeguru <codeguru@users.sourceforge.net>
2014-01-21 17:10:18 -0700

    Create project OverrideOnClickListener

---
+++ OverrideOnClickListener/src/codeguru/onclicklistener/MainActivity.java
@@ -0,0 +1,35 @@
+package codeguru.onclicklistener;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		View textView = findViewById(R.id.hello);
+		textView.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "MainActivity",
+						Toast.LENGTH_LONG).show();
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
