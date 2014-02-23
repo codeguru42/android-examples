618bccf94bb83bce9990ebc4eb56620eeb1acaf5
codeguru <codeguru@users.sourceforge.net>
2014-02-22 22:11:28 -0700

    Load a map

--- GoogleMapsExample/main/src/codeguru/maps/MainActivity.java
+++ GoogleMapsExample/main/src/codeguru/maps/MainActivity.java
@@ -1,10 +1,10 @@
 package codeguru.maps;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
