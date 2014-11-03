984f31ad54acee0878cd2c17e5e4f1bdb0350c2d
Code Guru <codeguru42@gmail.com>
2014-11-02 22:45:00 -0700

    LabeledEditText: Add label attribute

--- LabeledEditText/app/src/main/java/codeguru/labelededittext/LabeledEditText.java
+++ LabeledEditText/app/src/main/java/codeguru/labelededittext/LabeledEditText.java
@@ -1,11 +1,17 @@
 package codeguru.labelededittext;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class LabeledEditText extends LinearLayout {
+
+    @InjectView(R.id.label) TextView label;
 
     public LabeledEditText(Context context) {
         this(context, null);
@@ -18,7 +24,17 @@ public class LabeledEditText extends LinearLayout {
     public LabeledEditText(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
 
-        View.inflate(context, R.layout.labeled_edit_text, this);
+        View root = View.inflate(context, R.layout.labeled_edit_text, this);
+        ButterKnife.inject(this, root);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.LabeledEditText,
+                0, 0);
+
+        try {
+            label.setText(a.getString(R.styleable.LabeledEditText_label));
+        } finally {
+            a.recycle();
+        }
     }
 
 }
