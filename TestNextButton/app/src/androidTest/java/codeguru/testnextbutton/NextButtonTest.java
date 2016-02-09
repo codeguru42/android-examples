58ef88ec853b68bcaae7eecf8ab1ada8560cc5b4
Code Apprentice <codeguru42@gmail.com>
2016-02-09 15:54:28 -0700

    TestNextKey: Finish implementing test

--- TestNextButton/app/src/androidTest/java/codeguru/testnextbutton/NextButtonTest.java
+++ TestNextButton/app/src/androidTest/java/codeguru/testnextbutton/NextButtonTest.java
@@ -1,8 +1,12 @@
 package codeguru.testnextbutton;
 
+import android.app.Instrumentation;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,8 +21,18 @@ public class NextButtonTest {
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
 
+    private UiDevice device;
+
+    @Before
+    public void setUp() {
+        Instrumentation inst = InstrumentationRegistry.getInstrumentation();
+        device = UiDevice.getInstance(inst);
+    }
+
     @Test
     public void testNextButton() {
         onView(withId(R.id.first_text)).check(matches(hasFocus()));
+        device.pressEnter();
+        onView(withId(R.id.second_text)).check(matches(hasFocus()));
     }
 }
