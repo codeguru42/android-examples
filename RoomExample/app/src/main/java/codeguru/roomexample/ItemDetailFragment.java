0e97f56fbc1a750281e7c447e041ecd74355219b
Code Apprentice <codeguru42@gmail.com>
2018-08-11 16:50:21 -0600

    Remove unnecessary casts

--- RoomExample/app/src/main/java/codeguru/roomexample/ItemDetailFragment.java
+++ RoomExample/app/src/main/java/codeguru/roomexample/ItemDetailFragment.java
@@ -47,7 +47,7 @@ public class ItemDetailFragment extends Fragment {
             mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
 
             Activity activity = this.getActivity();
-            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            CollapsingToolbarLayout appBarLayout = activity.findViewById(R.id.toolbar_layout);
             if (appBarLayout != null) {
                 appBarLayout.setTitle(mItem.content);
             }
