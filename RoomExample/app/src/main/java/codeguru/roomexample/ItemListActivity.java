e5dda96182644204de6034a196fba7ab426fdbf0
Code Apprentice <codeguru42@gmail.com>
2018-08-12 16:01:42 -0600

    Use Room and LiveData to populate the RecyclerView

--- RoomExample/app/src/main/java/codeguru/roomexample/ItemListActivity.java
+++ RoomExample/app/src/main/java/codeguru/roomexample/ItemListActivity.java
@@ -4,22 +4,19 @@ import android.arch.lifecycle.LiveData;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.List;
+
 import codeguru.roomexample.database.ExampleDatabase;
 import codeguru.roomexample.database.Item;
-import codeguru.roomexample.dummy.DummyContent;
-
-import java.util.List;
 
 /**
  * An activity representing a list of Items. This activity
