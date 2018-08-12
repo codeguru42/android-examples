721cd8ace466f43766c6e5bc9a0f69a63a6829f5
Code Apprentice <codeguru42@gmail.com>
2018-08-12 10:52:26 -0600

    ExampleDatabase is now a singleton

--- RoomExample/app/src/main/java/codeguru/roomexample/database/ExampleDatabase.java
+++ RoomExample/app/src/main/java/codeguru/roomexample/database/ExampleDatabase.java
@@ -1,9 +1,23 @@
 package codeguru.roomexample.database;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 @Database(entities = Item.class, version = 1)
 public abstract class ExampleDatabase extends RoomDatabase {
+    static ExampleDatabase instance;
+
+    public static ExampleDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context, ExampleDatabase.class, "room-example.db").build();
+        }
+
+        return instance;
+    }
+
+    private ExampleDatabase() {}
+
     public abstract ItemDao exampleDao();
 }
