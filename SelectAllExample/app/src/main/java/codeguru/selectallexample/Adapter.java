ccc3eb9895f403a109b31e47910c42beeae5fdd5
Code Apprentice <codeguru42@gmail.com>
2018-11-04 08:14:21 -0700

    Port to AndroidX

--- SelectAllExample/app/src/main/java/codeguru/selectallexample/Adapter.java
+++ SelectAllExample/app/src/main/java/codeguru/selectallexample/Adapter.java
@@ -1,11 +1,12 @@
 package codeguru.selectallexample;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     private String[] data;
--- SelectAllExample/app/src/main/java/codeguru/selectallexample/MainActivity.java
+++ SelectAllExample/app/src/main/java/codeguru/selectallexample/MainActivity.java
@@ -1,9 +1,10 @@
 package codeguru.selectallexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
