33eca54a5e33928d9ec146cf0dc28c9bf7608f77
Code-Apprentice <codeguru42@gmail.com>
2023-07-02 21:27:18 -0600

    Use view binding

--- NotificationExample/app/build.gradle.kts
+++ NotificationExample/app/build.gradle.kts
@@ -37,6 +37,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
--- NotificationExample/app/src/main/java/codeguru/notificationexample/MainActivity.kt
+++ NotificationExample/app/src/main/java/codeguru/notificationexample/MainActivity.kt
@@ -2,10 +2,12 @@ package codeguru.notificationexample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import codeguru.notificationexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
