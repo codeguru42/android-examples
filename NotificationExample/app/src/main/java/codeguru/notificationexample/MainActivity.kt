d338163929472733132579a55235760bac5cde41
Code-Apprentice <codeguru42@gmail.com>
2023-07-03 01:07:09 -0600

    Request notification permission

--- NotificationExample/app/src/main/java/codeguru/notificationexample/MainActivity.kt
+++ NotificationExample/app/src/main/java/codeguru/notificationexample/MainActivity.kt
@@ -1,18 +1,71 @@
 package codeguru.notificationexample
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import codeguru.notificationexample.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Snackbar.make(binding.root, "Permission Granted", Snackbar.LENGTH_LONG)
+                    .show()
+            } else {
+                // Explain to the user that the feature is unavailable because the
+                // feature requires a permission that the user has denied. At the
+                // same time, respect the user's decision. Don't link to system
+                // settings in an effort to convince the user to change their
+                // decision.
+                Snackbar.make(binding.root, "Unable to show notification", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.notificationButton.setOnClickListener {
-            Snackbar.make(binding.root, "Button clicked", Snackbar.LENGTH_LONG).show()
+            requestPermission()
+        }
+    }
+
+    private fun requestPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // You can use the API that requires the permission.
+                Snackbar.make(binding.root, "Permission already granted", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+
+            shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) -> {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected, and what
+                // features are disabled if it's declined. In this UI, include a
+                // "cancel" or "no thanks" button that lets the user continue
+                // using your app without granting the permission.
+                Snackbar.make(binding.root, "I want to show notifications", Snackbar.LENGTH_LONG)
+                    .show()
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
         }
     }
 }
