45aed65e32a5bb40a25bd1aab226465067d07652
Code-Apprentice <codeguru42@gmail.com>
2023-07-02 21:34:37 -0600

    Add click listener

--- NotificationExample/app/src/main/java/codeguru/notificationexample/MainActivity.kt
+++ NotificationExample/app/src/main/java/codeguru/notificationexample/MainActivity.kt
@@ -3,11 +3,16 @@ package codeguru.notificationexample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import codeguru.notificationexample.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.notificationButton.setOnClickListener {
+            Snackbar.make(binding.root, "Button clicked", Snackbar.LENGTH_LONG).show()
+        }
     }
 }
