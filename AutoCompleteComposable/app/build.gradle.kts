4c5decbf2a97fe287d94a3c3d14af51fa7b9b8f6
Code-Apprentice <codeguru42@gmail.com>
2024-11-24 15:33:35 -0700

    SDK 35

--- AutoCompleteComposable/app/build.gradle.kts
+++ AutoCompleteComposable/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.autocompletecomposable"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.autocompletecomposable"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
