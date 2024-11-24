73a70ecf25e68f3dc6ae7e531216e6c658988fb0
Code-Apprentice <codeguru42@gmail.com>
2024-11-24 16:17:36 -0700

    Clean up generated code

--- AutoCompleteComposable/app/src/main/java/com/example/autocompletecomposable/MainActivity.kt
+++ AutoCompleteComposable/app/src/main/java/com/example/autocompletecomposable/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.autocompletecomposable.ui.theme.AutoCompleteComposableTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,21 +15,5 @@ class MainActivity : ComponentActivity() {
                 AutoCompleteExample()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AutoCompleteComposableTheme {
-        Greeting("Android")
     }
 }
